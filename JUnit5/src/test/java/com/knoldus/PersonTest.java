64c314b63d361a1deb1859fbd1986f42c117c0f4
SahilBabbar12 <sahil.babbar@knoldus.com>
2023-04-11 22:27:13 +0530

    updated method names with required naming conventions

--- JUnit5/src/test/java/com/knoldus/PersonTest.java
+++ JUnit5/src/test/java/com/knoldus/PersonTest.java
@@ -2,6 +2,7 @@ package com.knoldus;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
@@ -17,66 +18,64 @@ public class PersonTest {
         person = new Person(name, age, email);
     }
 
-    // Test to set and getName with not-null value.
     @Test
-    void getNameTest1() {
+    @DisplayName("Test to set and getName with not-null value")
+    void testgetName_given_nonNullValue() {
         person.setName("Sahil Babbar");
         Assertions.assertEquals("Sahil Babbar", person.getName());
     }
 
-
-    // Test to set and getName with null value.
     @Test
-    void getNameTest2() {
+    @DisplayName("Test to set and getName with null value")
+    void testgetName_given_nullValue() {
         person.setName(null);
         Assertions.assertNull(person.getName());
     }
 
-    // Test to  set and getAge with not-null value.
     @Test
-    void getAgeTest1() {
+    @DisplayName("Test to set and getAge with not-null value")
+    void testgetAge_given_nonNullValue() {
         person.setAge(20);
         Assertions.assertEquals(20, person.getAge());
     }
 
-    // Test to  set and getAge with null value.
     @Test
-
-    void getAgeTest2() {
+    @DisplayName("Test to set and getAge with null value")
+    void testgetAge_given_nullValue() {
         person.setAge(0);
         Assertions.assertEquals(0, person.getAge());
     }
 
-    // Test to set and getEmail with non-null value
     @Test
-    void getEmailTest1() {
+    @DisplayName("Test to set and getEmail with non-null value")
+    void testgetEmail_given_nonNullValue() {
         person.setEmail("Sahil.Babbar@knoldus.com");
         Assertions.assertEquals("Sahil.Babbar@knoldus.com", person.getEmail());
     }
 
-    //Test to set and getEmail with null value.
     @Test
-
-    void getEmailTest2() {
+    @DisplayName("Test to set and getEmail with null value")
+    void testgetEmail_given_nullValue() {
         person.setEmail(null);
         Assertions.assertNull(person.getEmail());
     }
 
-    // Test to get and setAge with positive integer
     @Test
-    void setAgeTestForPositiveNumber() {
+    @DisplayName("Test to get and setAge with positive integer")
+    void testgetAge_given_PositiveNumber() {
         person.setAge(23);
         Assertions.assertEquals(23, person.getAge());
     }
 
-    // Test to get and setAge with negative integer
     @Test
-    void setAgeTestForNegativeNumber() {
+    @DisplayName("Test to get and setAge with negative integer")
+    void testgetAge_given_NegativeNumber() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> person.setAge(-11));
     }
 
     @Test
-    public void setAgeTest_ifAgeIsZero() {
+    @DisplayName("Test to get and setAge with zero")
+    public void testgetAge_givenAgeIsZero() {
         int age = 0;
         person.setAge(age);
         Assertions.assertEquals(age, person.getAge());

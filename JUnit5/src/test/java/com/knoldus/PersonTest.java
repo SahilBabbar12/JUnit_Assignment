6c11dc456e0ef7072d55278b891e184a89104fd3
SahilBabbar12 <sahil.babbar@knoldus.com>
2023-04-11 23:43:16 +0530

    updated methods with required naming conventions

--- JUnit5/src/test/java/com/knoldus/PersonTest.java
+++ JUnit5/src/test/java/com/knoldus/PersonTest.java
@@ -20,62 +20,62 @@ public class PersonTest {
 
     @Test
     @DisplayName("Test to set and getName with not-null value")
-    void testgetName_given_nonNullValue() {
+    void testgetName_givenNonNullValue_shouldReturnInputName() {
         person.setName("Sahil Babbar");
         Assertions.assertEquals("Sahil Babbar", person.getName());
     }
 
     @Test
     @DisplayName("Test to set and getName with null value")
-    void testgetName_given_nullValue() {
+    void testgetName_givenNullValue_shouldReturnNull() {
         person.setName(null);
         Assertions.assertNull(person.getName());
     }
 
     @Test
     @DisplayName("Test to set and getAge with not-null value")
-    void testgetAge_given_nonNullValue() {
+    void testgetAge_givenNonNullValue_shouldReturnInputAge() {
         person.setAge(20);
         Assertions.assertEquals(20, person.getAge());
     }
 
     @Test
     @DisplayName("Test to set and getAge with null value")
-    void testgetAge_given_nullValue() {
+    void testgetAge_givenNullValue_shouldReturnNull() {
         person.setAge(0);
         Assertions.assertEquals(0, person.getAge());
     }
 
     @Test
     @DisplayName("Test to set and getEmail with non-null value")
-    void testgetEmail_given_nonNullValue() {
+    void testgetEmail_givenNonNullValue_shouldReturnInputEmail() {
         person.setEmail("Sahil.Babbar@knoldus.com");
         Assertions.assertEquals("Sahil.Babbar@knoldus.com", person.getEmail());
     }
 
     @Test
     @DisplayName("Test to set and getEmail with null value")
-    void testgetEmail_given_nullValue() {
+    void testgetEmail_givenNnullValue_shouldReturnNull() {
         person.setEmail(null);
         Assertions.assertNull(person.getEmail());
     }
 
     @Test
     @DisplayName("Test to get and setAge with positive integer")
-    void testgetAge_given_PositiveNumber() {
+    void testsetAge_givenPositiveNumber_shouldSetInputAge() {
         person.setAge(23);
         Assertions.assertEquals(23, person.getAge());
     }
 
     @Test
     @DisplayName("Test to get and setAge with negative integer")
-    void testgetAge_given_NegativeNumber() {
+    void testsetAge_givenNegativeNumber_shouldThrow_IllegalArgumentException() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> person.setAge(-11));
     }
 
     @Test
     @DisplayName("Test to get and setAge with zero")
-    public void testgetAge_givenAgeIsZero() {
+    public void testsetAge_givenAgeIsZero_shouldSetAgeAsZero() {
         int age = 0;
         person.setAge(age);
         Assertions.assertEquals(age, person.getAge());
